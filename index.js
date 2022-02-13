30cc06fb8f86ced830aa19044f4db44406b5eeec
sang2xx2 <s191102g@gmail.com>
2022-02-13 15:47:22 +0700

    change port

--- index.js
+++ index.js
@@ -23,7 +23,12 @@ server.use((req, res, next) => {
 })
 
 // Use default router
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+// server.use(router)
+// server.listen(3000, () => {
+//   console.log('JSON Server is running')
+// })
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
